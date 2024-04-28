471c902810e586f9250f02817e747aeb40ed257a
Christian Köhnenkamp <cvk5@me.com>
2024-04-28 14:43:22 +0200

    Remove DB bean population

--- src/main/java/com/example/webhello/LoadDatabase.java
+++ src/main/java/com/example/webhello/LoadDatabase.java
@@ -3,8 +3,8 @@ package com.example.webhello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,12 +12,14 @@ class LoadDatabase {
 
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
+  /*
   @Bean
   CommandLineRunner initDatabase(EmployeeRepository repository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "CEO")));
+      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "CFO")));
+      log.info("Preloading " + repository.save(new Employee("Lars Thylen", "CTO")));
     };
-  }
+  } */
 }
