09453cf9cbe2eac7fd19b7c40dd9e6ab8cba9791
Christian Köhnenkamp <cvk5@me.com>
2024-04-29 12:59:59 +0200

    Update data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO EMPLOYEE (id, name, role) VALUES(4, 'Hello 4', 'World');
-INSERT INTO EMPLOYEE (id, name, role) VALUES(5, 'Hello 5', 'World');
-INSERT INTO EMPLOYEE (id, name, role) VALUES(6, 'Hello 5', 'World');
+INSERT INTO EMPLOYEE (name, role) VALUES('Hello', 'World');
+INSERT INTO EMPLOYEE (name, role) VALUES('Lars', 'CTO');
+INSERT INTO EMPLOYEE (name, role) VALUES('Magnus', 'Politico');
